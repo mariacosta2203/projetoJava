56fd246019686602f0a943778ea13fbedc2cb144
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2024-12-10 10:23:10 -0300

    exemplos metodos

--- src/metodos/ExemploMetodoExterno.java
+++ src/metodos/ExemploMetodoExterno.java
@@ -5,7 +5,7 @@ public class ExemploMetodoExterno {
 	//definição(estruturando) do metodo falar - método sem retorno e sem parametro
 	public static void mensagem() {
 		
-		System.out.println("Hello WOrd, bem-vindo turma 79");
+		System.out.println("Hello Word, bem-vindo turma 79");
 	}
 
 }
